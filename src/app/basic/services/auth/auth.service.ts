da760ead32f29d3e3bf36b6b32468000f0198e3f
chunkityip <alex0917lfo@hotmail.com>
2024-03-25 14:41:29 -0400

    Creating Sign-up Company Rest API Call

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:4200/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  registerClient(signupRequestDTO: any) : Observable<any> {
+    return this.http.post(BASIC_URL + "/client/sign-up" , signupRequestDTO);
+  }
 }
